b3f6663cd05ece596c851caab218d5c2bfdefd70
Jeppe Faber <JeppestougaardFaber@gmail.com>
2019-03-10 21:27:56 +0100

    Point cloud experiments

---
+++ Project Walnut - Shader Playground/Assets/_Scripts/Debug_CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Debug_CameraMovement : MonoBehaviour
+{
+    public float Speed = 50;
+
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * Speed;
+        float zAxisValue = Input.GetAxis("Vertical") * Speed;
+        float yValue = 0.0f;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            yValue = -Speed;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            yValue = Speed;
+        }
+
+        transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y + yValue, transform.position.z + zAxisValue);
+    }
+}
---
+++ Project Walnut - Shader Playground/Assets/_Scripts/PointCloudMeshGen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PointCloudMeshGen : MonoBehaviour
+{
+
+    private Mesh mesh;
+    public int numPoints = 50;
+
+    // Use this for initialization
+    void Start()
+    {
+        mesh = new Mesh();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateMesh();
+    }
+
+    void CreateMesh()
+    {
+        Vector3[] points = new Vector3[numPoints];
+        int[] indecies = new int[numPoints];
+        Color[] colors = new Color[numPoints];
+        for (int i = 0; i < points.Length; ++i)
+        {
+            points[i] = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            indecies[i] = i;
+            colors[i] = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+        }
+
+        mesh.vertices = points;
+        mesh.colors = colors;
+        mesh.SetIndices(indecies, MeshTopology.Points, 0);
+
+    }
+}
